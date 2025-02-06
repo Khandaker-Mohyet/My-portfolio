ebc5b9917f3de13c58c40ecb60c783d34e618907
Khandaker-Mohyet <mohyet008@gmail.com>
2025-02-06 21:26:52 +0600

    first commit

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,34 @@
+import { CiLinkedin } from 'react-icons/ci';
+import image from '../assets/image.png'
+import { FaDownload, FaFacebook, FaGithub } from 'react-icons/fa';
+
+const Banner = () => {
+  return (
+    <div className="w-10/12 mx-auto border-2 text-white">
+      <div className="flex">
+        {/* deatils */}
+        <div className='flex-1'>
+          <div className=''>
+            <h1 className='text-4xl font-semibold'>helow </h1>
+            <h1 className='text-4xl font-semibold'>This is Abdullah Al Mohyet</h1>
+            <h1 className='text-2xl font-semibold'>I am a Frontand Developer</h1>
+          </div>
+          <div className='flex'>
+            <button><CiLinkedin className='size-12' /></button>
+            <button><FaGithub className='size-12'/></button>
+            <button><FaFacebook className='size-12'/></button>
+          </div>
+          <button><FaDownload /> Download Resume</button>
+        </div>
+
+
+        {/* image */}
+        <div className='border-2 flex-1'>
+          <img className='w-[350px] h-[350px] rounded-full text-center' src={image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
