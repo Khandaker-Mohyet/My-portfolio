05694130fe89051e5c7f9d4647f090306418fa7b
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-03 02:46:42 +0600

    change about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,10 +9,10 @@ const About = () => {
             About Me
           </h2>
           <p className="text-gray-300 leading-relaxed text-2xl">
-          I'm <span className="font-bold text-white ">Abdullah Al Mohyet</span>, a passionate Fontend developer specializing React ,Tailwind ,Express mongoDb My focus is on making web development faster, easier, and accessible to all developers. Currently, I’m expanding into backend development to grow as a Fontend development and create seamless, robust web applications.
+          I’m <span className="font-bold text-white ">Khandaker Mohyet</span>, a passionate MERN stack developer with a strong focus on building fast, responsive, and user-friendly web applications. I specialize in React, Tailwind CSS, Express.js, and MongoDB, and I’m committed to making web development smoother and more accessible for developers.
           <br />
           <br />
-          I’m a lifelong learner and innovator, driven by a desire to contribute to the developer community with new ideas and tools that deliver real value.
+          While my core strength lies in frontend development, I’m actively expanding my skills in backend technologies to deliver more complete and seamless full-stack solutions. I’m a lifelong learner and innovator, always eager to grow and contribute to the developer community by creating tools and experiences that provide real value.
           </p>
         </div>
       </div>
