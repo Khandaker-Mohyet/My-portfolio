c03133e1dcaebf6c63cede9fbea54f59d670f418
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-03 17:31:52 +0600

    change resume

--- src/Components/BannerNew.jsx
+++ src/Components/BannerNew.jsx
@@ -31,7 +31,7 @@ const BannerNew = () => {
           </div>
 
           {/* Download Resume Button */}
-          <a href="/resume-M.pdf"
+          <a href="/public/Mohyet_resume.pdf"
             download
             className='inline-block'
           >
