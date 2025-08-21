2017ee8e009810b7ecfa0637e5d812e90c1c2084
Khandaker-Mohyet <mohyet008@gmail.com>
2025-08-21 19:03:28 +0600

    add bootstrap and jqarry

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,12 +1,12 @@
 
 import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs } from 'react-icons/fa';
-import { SiTypescript, SiTailwindcss, SiMongodb, SiExpress, SiFirebase } from 'react-icons/si';
+import { SiTypescript, SiTailwindcss, SiMongodb, SiExpress, SiFirebase, SiBootstrap, SiJquery } from 'react-icons/si';
 
 
 const Skills = () => {
   return (
     <div id="skill" className="relative py-16 bg-background text-center overflow-hidden">
-      
+
       <h2 className="text-4xl font-extrabold text-center bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">All Skills</h2>
       <p className="text-neutral-500 dark:text-neutral-400 text-base font-normal leading-relaxed mb-8 relative z-10">
         Here are some of the key technologies and tools I work with regularly.
@@ -15,6 +15,8 @@ const Skills = () => {
         <FaHtml5 className="text-6xl text-orange-600" />
         <FaCss3Alt className="text-6xl text-blue-600" />
         <SiTailwindcss className="text-6xl text-teal-500" />
+        <SiBootstrap className="text-6xl text-purple-600" />
+        <SiJquery className="text-6xl text-blue-700" />
         <FaJsSquare className="text-6xl text-yellow-500" />
         <SiTypescript className="text-6xl text-blue-500" />
         <FaReact className="text-6xl text-blue-400" />
